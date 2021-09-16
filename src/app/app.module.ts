4824f8e7f2a08b0565faee8530e463602deba8af
eforinger <ef0042@mix.wvu.edu>
2021-09-16 16:53:17 -0400

    update files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { Carousel2Component } from './carousel2/carousel2.component';
 import { FooterComponent } from './footer/footer.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { LayoutMainComponent } from './layout/layout-main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    FooterComponent
+    FooterComponent,
+    CarouselComponent,
+    LayoutMainComponent,
+    CardComponent,
+    Carousel2Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/carousel2/carousel2.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-carousel2',
+    templateUrl: 'carousel2.component.html'
+})
+export class Carousel2Component {
+
+}
