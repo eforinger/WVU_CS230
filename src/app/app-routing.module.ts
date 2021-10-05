ec6650447d16ef5be05e412d59f34b7a8f44bfe8
eforinger <ef0042@mix.wvu.edu>
2021-10-05 18:19:15 -0400

    update files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './body/home.component';
+import { HubsComponent } from './body/hubs.component';
 import { MoviesComponent } from './body/movies.component';
+import { MystuffComponent } from './body/mystuff.component';
 import { SearchComponent } from './body/search.component';
 import { SportsComponent } from './body/sports.component';
+import { TvComponent } from './body/tv.component';
 import { LayoutMainComponent } from './layout/layout-main.component';
 
 const routes: Routes = [
   { path: 'home', component: LayoutMainComponent },
+  { path: 'tv', component: TvComponent },
   { path: 'movies', component: MoviesComponent },
   { path: 'search', component: SearchComponent },
   { path: 'sports', component: SportsComponent },
-  { path: '', component: HomeComponent }
+  { path: 'mystuff', component: MystuffComponent },
+  { path: 'hubs', component: HubsComponent}
 ];
 
 @NgModule({
--- src/app/body/movies.component.ts
+++ src/app/body/movies.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'hulu-movies',
   templateUrl: './movies.component.html',
+  styleUrls: ['movies.component.css']
 })
 export class MoviesComponent implements OnInit {
 
