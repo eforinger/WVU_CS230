13842e565f74ddbc58361e400406369cef466154
eforinger <ef0042@mix.wvu.edu>
2021-10-21 14:57:03 -0400

    updated file

---
+++ src/app/header/user-info.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { UserInfo } from "./user-info.model";
+
+@Injectable({providedIn: 'root'})
+export class UserInfoService {
+    private baseUrl: string = 'https://hulu-app-67b3e-default-rtdb.firebaseio.com/';
+    private myInfoEndpoint:string = 'my-info.json';
+    constructor(private http: HttpClient){
+
+    }
+
+    getUserInfo(){
+        console.log()
+        return this.http.get<UserInfo>(this.baseUrl + this.myInfoEndpoint);
+    }
+}
